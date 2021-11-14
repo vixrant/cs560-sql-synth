999ef8b65a7f50740b6899c81fc1ad3dbafeaea2
Vikrant Gajria <vikrantgajria@google.com>
2021-11-14 12:32:10 -0500

    Restructure

--- Program.cs
+++ Program.cs
@@ -12,11 +12,13 @@ using Microsoft.ProgramSynthesis.Specifications;
 using Microsoft.ProgramSynthesis.VersionSpace;
 using Newtonsoft.Json;
 using System.Text.RegularExpressions;
-public class TableSchema {
+public class TableSchema
+{
     public string name { get; set; }
     public IList<string> columns { get; set; }
 }
-public class ExampleSchema {
+public class ExampleSchema
+{
     public IList<TableSchema> inputs { get; set; }
     public IList<string> output { get; set; }
 }
@@ -35,57 +37,97 @@ namespace ProseTutorial
 
         private static readonly Dictionary<State, object> Examples = new Dictionary<State, object>();
         private static ProgramNode _topProgram;
+        public static SynthesisEngine ConfigureSynthesis()
+        {
+            var witnessFunctions = new WitnessFunctions(Grammar);
+            var deductiveSynthesis = new DeductiveSynthesis(witnessFunctions);
+            var synthesisExtrategies = new ISynthesisStrategy[] { deductiveSynthesis };
+            var synthesisConfig = new SynthesisEngine.Config { Strategies = synthesisExtrategies };
+            var prose = new SynthesisEngine(Grammar, synthesisConfig);
+            return prose;
+        }
 
-        private static List<string[]> parsefile(string filename) {
+        private static List<string[]> parsefile(string filename)
+        {
             List<string[]> filecontents = new List<string[]>();
-            using (StreamReader reader = new StreamReader(filename)) {
+            using (StreamReader reader = new StreamReader(filename))
+            {
                 string line;
-                while ((line = reader.ReadLine()) != null) {
+                while ((line = reader.ReadLine()) != null)
+                {
                     Regex CSVParser = new Regex(",(?=(?:[^\"]*\"[^\"]*\")*(?![^\"]*\"))");
                     var arr = CSVParser.Split(line);
-                    for (int i=0;i<arr.Length;i++) arr[i]=arr[i].Trim();
-                    if (filecontents.Count==0 || arr.Length==filecontents[0].Length)
+                    for (int i = 0; i < arr.Length; i++) arr[i] = arr[i].Trim();
+                    if (filecontents.Count == 0 || arr.Length == filecontents[0].Length)
                         filecontents.Add(CSVParser.Split(line));
                 }
             }
             return filecontents;
         }
-        private static void Main(string[] args) {
-            _prose = ConfigureSynthesis();
+
+        /**
+        Program can take input either from command line or using a prompt.
+        If the input directory doesn't exist, or the schema.json file is missing, then
+        we throw an InvalidOperationException to notify that the we cannot synthesize
+        a program for that directory.
+        */
+        private static string getInput(string[] args)
+        {
             string input;
-            //input arguments seems to be 0-indexed when using dotnet run... weird...
-            if (args.Length>0) { input = args[0]; } else {
+
+            if (args.Length > 0)
+                input = args[0];
+            else
+            {
                 Console.Out.WriteLine("Please specify a path to a directory containing a schema and some examples");
                 input = Console.ReadLine();
             }
-            // input = Path.Combine(new String[] {System.IO.Directory.GetCurrentDirectory(),input});
-            if (!Directory.Exists(input)) { Console.Out.WriteLine("Invalid input: {0} isn't a directory",input);return;}
-            string schemapath = Path.Combine(new String[] {input,"schema.json"});
-            if (!File.Exists(schemapath)) { Console.Out.WriteLine("Invalid input: {0} doesn't exist",schemapath);return;}
+
+            // input = Path.Combine(new String[] {System.IO.Directory.GetCurrentDirectory(), input});
+
+            if (!Directory.Exists(input))
+                throw new InvalidOperationException("Invalid input: {0} isn't a directory" + input);
+
+            string schemapath = Path.Combine(new String[] { input, "schema.json" });
+
+            if (!File.Exists(schemapath))
+                throw new InvalidOperationException("Invalid input: {0} doesn't exist" + schemapath);
+
+            return input;
+        }
+
+        private static void Main(string[] args)
+        {
+            _prose = ConfigureSynthesis();
+            var input = getInput(args);
+
             ExampleSchema schema = JsonConvert.DeserializeObject<ExampleSchema>(File.ReadAllText(schemapath));
-            foreach (string subfile in Directory.GetDirectories(input)) {
+            foreach (string subfile in Directory.GetDirectories(input))
+            {
                 List<List<string[]>> intables = new List<List<string[]>>();
-                foreach (TableSchema tableschema in schema.inputs) {
-                    string inputpath = Path.Combine(new String[] {subfile,"input_tables",tableschema.name+".csv"});
-                    if (!File.Exists(inputpath)) { Console.Out.WriteLine("Required file not found: {0} doesn't exist",inputpath);return;}
+                foreach (TableSchema tableschema in schema.inputs)
+                {
+                    string inputpath = Path.Combine(new String[] { subfile, "input_tables", tableschema.name + ".csv" });
+                    if (!File.Exists(inputpath)) { Console.Out.WriteLine("Required file not found: {0} doesn't exist", inputpath); return; }
                     intables.Add(parsefile(inputpath));
                 }
-                string outputpath = Path.Combine(new String[] {subfile,"output_table.csv"});
-                if (!File.Exists(outputpath)) { Console.Out.WriteLine("Required file not found: {0} doesn't exist",outputpath);return;}
+                string outputpath = Path.Combine(new String[] { subfile, "output_table.csv" });
+                if (!File.Exists(outputpath)) { Console.Out.WriteLine("Required file not found: {0} doesn't exist", outputpath); return; }
                 List<string[]> outable = parsefile(outputpath);
                 State inputState = State.CreateForExecution(Grammar.InputSymbol, intables);
                 Examples.Add(inputState, outable);
             }
 
             var spec = new ExampleSpec(Examples);
-            Console.Out.WriteLine("Learning a program for {0} examples...",Examples.Count);
+            Console.Out.WriteLine("Learning a program for {0} examples...", Examples.Count);
             var scoreFeature = new RankingScore(Grammar);
             ProgramSet topPrograms = _prose.LearnGrammarTopK(spec, scoreFeature, 1, null);
             if (topPrograms.IsEmpty) throw new Exception("No program was found for this specification.");
             _topProgram = topPrograms.RealizedPrograms.First();
             Console.Out.WriteLine("Top 4 learned programs:");
             var counter = 1;
-            foreach (ProgramNode program in topPrograms.RealizedPrograms) {
+            foreach (ProgramNode program in topPrograms.RealizedPrograms)
+            {
                 if (counter > 4) break;
                 Console.Out.WriteLine("==========================");
                 Console.Out.WriteLine("Program {0}: ", counter);
@@ -117,16 +159,6 @@ namespace ProseTutorial
             {
                 throw new Exception("The execution of the program on this input thrown an exception");
             }
-        }
-
-        public static SynthesisEngine ConfigureSynthesis()
-        {
-            var witnessFunctions = new WitnessFunctions(Grammar);
-            var deductiveSynthesis = new DeductiveSynthesis(witnessFunctions);
-            var synthesisExtrategies = new ISynthesisStrategy[] {deductiveSynthesis};
-            var synthesisConfig = new SynthesisEngine.Config {Strategies = synthesisExtrategies};
-            var prose = new SynthesisEngine(Grammar, synthesisConfig);
-            return prose;
         }
     }
 }
